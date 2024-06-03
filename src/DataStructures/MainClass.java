f84ae5cbad44132cabc194124a21f6a215fa6fd9
Marika-Kankia <marikakankia777@gmail.com>
2024-06-03 21:17:51 +0400

    Modified 6th assignment

--- src/DataStructures/MainClass.java
+++ src/DataStructures/MainClass.java
@@ -3,31 +3,32 @@ package DataStructures;
 import java.util.ArrayList;
 
 import static DataStructures.WordArray.findDigit;
-import static DataStructures.WordArray.printArrayList;
+
 
 public class MainClass {
 
 public static void main(String[] args) {
-        // ArrayList-ს შექმნა
-        ArrayList<String> words = new ArrayList<>();
 
-        // 10 სიტყვის დამატება
-        words.add("Num1");
-        words.add("Num2");
-        words.add("Num3");
-        words.add("Num4");
-        words.add("Num5");
-        words.add("Num6");
-        words.add("Num7");
-        words.add("Num8");
-        words.add("Num9");
-        words.add("Num10");
+        String[] words = new String[10];
 
-        // თითოეული სიტყვის დაბეჭდვა for loop-ის გამოყენებით
-//        for (int i = 0; i < words.size(); i++) {
-//        System.out.println("Number" +" "+ (i + 1) +" "+ "Is : " + words.get(i));
-//        }
-        printArrayList(words);
+        // 10 სიტყვის დამატება მასივში
+        words[0] = "word1";
+        words[1] = "word2";
+        words[2] = "word3";
+        words[3] = "word4";
+        words[4] = "word5";
+        words[5] = "word6";
+        words[6] = "word7";
+        words[7] = "word8";
+        words[8] = "word9";
+        words[9] = "word10";
+
+
+ //        თითოეული სიტყვის დაბეჭდვა for loop-ის გამოყენებით
+        for (int i = 0; i < words.length; i++) {
+          System.out.println("Word #" + (i + 1) + " in the massive is: \"" + words[i] + "\"");
+        }
+
 
 // ციფრის პოვნა მასივში
 
--- src/DataStructures/WordArray.java
+++ src/DataStructures/WordArray.java
@@ -2,16 +2,11 @@ package DataStructures;
 import java.util.ArrayList;
 class WordArray{
 
-    public static void printArrayList(ArrayList<String> list) {
-        for (String item : list) {
-            System.out.println(item);
-        }
-    }
     public static void findDigit(int[] arr, int digit) {
             boolean found = false;
             for (int i = 0; i < arr.length; i++) {
                 if (arr[i] == digit) {
-                    System.out.println("The Index of number " +  digit + " "+ "is: " + i);
+                    System.out.println("The Index of the number " +  digit + " "+ "is: " + i);
                     found = true;
                     break;
                 }
